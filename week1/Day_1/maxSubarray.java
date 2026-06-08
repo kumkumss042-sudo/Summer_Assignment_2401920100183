99868833fd2d6ff5915cad00e591a07da0924ab0
kumkum_20 <kumkumss042@gmail.com>
2026-06-08 20:51:07 +0530

    updated code for validAnagram.java

--- week1/Day_1/maxSubarray.java
+++ week1/Day_1/maxSubarray.java
@@ -1,4 +1,4 @@
-package Summer_Assignment_2401920100183.week1.Day_1;
+package week1.Day_1;
 import java.util.*;
 
 public class maxSubarray{
---
+++ week2/Day_1/ransomNote.java
@@ -0,0 +1,18 @@
+package week2.Day_1;
+
+class Solution {
+    public boolean canConstruct(String r, String m) {
+        if(m.length()<r.length()) return false;
+        int freq[]=new int[26];
+        for(int i=0;i<m.length();i++){
+            freq[m.charAt(i)-'a']++;
+        }
+        for(int i=0;i<r.length();i++){
+            freq[r.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]<0) return false;
+        }
+        return true;
+    }
+}
