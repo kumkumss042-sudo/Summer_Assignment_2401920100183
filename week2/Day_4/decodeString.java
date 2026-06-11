2973edca4141ac9911da2232efba6caf1157a125
kumkum_20 <kumkumss042@gmail.com>
2026-06-11 23:49:48 +0530

    updated code for decodeString.java

--- week2/Day_4/decodeString.java
+++ week2/Day_4/decodeString.java
@@ -3,45 +3,36 @@ package week2.Day_4;
 import java.util.*;
 class Solution {
     public String decodeString(String s) {
+       Stack<Integer>stNum=new Stack<>();
+       Stack<String>stChar=new Stack<>();
 
-        Stack<Integer> count= new Stack<>();
-        Stack<String> string= new Stack<>();
-
-        String curr = "";
-        int num = 0;
-
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            }
-
-            else if (ch == '[') {
-                count.push(num);
-                string.push(curr);
-
-                num = 0;
-                curr = "";
-            }
-
-            else if (ch == ']') {
-
-                int repeat = count.pop();
-                String prev = string.pop();
-
-                StringBuilder sb = new StringBuilder(prev);
-
-                while (repeat-- > 0) {
-                    sb.append(curr);
+       int num=0;
+       for(int i=0;i<s.length();i++){
+          char ch=s.charAt(i);
+          if(Character.isDigit(ch)) num=num*10+(ch-'0');
+          else if(ch=='['){
+            stNum.push(num);
+            stChar.push("[");
+            num=0;
+          }
+          else if(ch==']'){
+            String ans="";
+              while(!stChar.peek().equals("[")) ans=stChar.pop()+ans;
+                int no=stNum.pop();
+                StringBuilder sb=new StringBuilder();
+                while(no-->0){
+                    sb.append(ans);
                 }
+                stChar.pop();
+                stChar.push(sb.toString());
+          }
+          else stChar.push(ch+"");
+       }
 
-                curr = sb.toString();
-            }
-
-            else {
-                curr += ch;
-            }
-        }
-
-        return curr;
+       StringBuilder res=new StringBuilder();
+       for(String str: stChar){
+        res.append(str);
+       }
+       return res.toString();
     }
 }
